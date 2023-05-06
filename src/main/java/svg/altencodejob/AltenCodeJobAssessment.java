6f281158c1437f5443ab4c1de2b7cf709bc3ade3
Sergi Vaqué Galdón <sergivaque@gmail.com>
2023-05-06 18:41:58 +0200

    Added controller implmented

--- src/main/java/svg/altencodejob/AltenCodeJobAssessment.java
+++ src/main/java/svg/altencodejob/AltenCodeJobAssessment.java
@@ -4,11 +4,18 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import svg.altencodejob.infra.adapters.input.PriceMapper;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Prices API", version = "1.0", description = "Prices API Information"))
 public class AltenCodeJobAssessment {
     public static void main(String[] args) {
         SpringApplication.run(AltenCodeJobAssessment.class, args);
+    }
+
+    @Bean
+    PriceMapper priceMapper() {
+        return PriceMapper.INSTANCE;
     }
 }
